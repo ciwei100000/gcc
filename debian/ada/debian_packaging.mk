01642de188aa3f9ae362939a9ac2c7cbd2f864cc
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2019-08-19 16:52:15 +0200

    ada: update packaging Makefile snippet for gcc-9
    Option --as-needed is now activated by default
    
    Advice to pass CC to configure instead of exporting it, which may
    cause unexpected interactions without trace in logs.
    
    Look DEB_BUILD_MAINT_OPTIONS in addition to DEB_BUILD_OPTIONS..

--- debian/ada/debian_packaging.mk
+++ debian/ada/debian_packaging.mk
@@ -1,6 +1,6 @@
 # Common settings for Ada Debian packaging.
 #
-#  Copyright (C) 2012-2018 Nicolas Boulenguez <nicolas@debian.org>
+#  Copyright (C) 2012-2019 Nicolas Boulenguez <nicolas@debian.org>
 #
 #  This program is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -13,16 +13,14 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 # Typical use:
 #
 # gnat_version := $(shell gnatgcc -dumpversion)
 # DEB_BUILD_MAINT_OPTIONS := hardening=+all
-# DEB_LDFLAGS_MAINT_APPEND := -Wl,--as-needed -Wl,--no-undefined -Wl,--no-copy-dt-needed-entries -Wl,--no-allow-shlib-undefined
+# DEB_LDFLAGS_MAINT_APPEND := -Wl,--no-undefined -Wl,--no-copy-dt-needed-entries -Wl,--no-allow-shlib-undefined
 # DEB_ADAFLAGS_MAINT_APPEND := -gnatwa -Wall
 # include /usr/share/dpkg/buildflags.mk
 # include /usr/share/ada/debian_packaging-$(gnat_version).mk
-
 
 # dpkg-dev provides /usr/share/dpkg/default.mk (or the
 # more specific buildflags.mk) to set standard variables like
@@ -58,41 +56,14 @@ ifdef DPKG_EXPORT_BUILDFLAGS
   export ADAFLAGS
 endif
 
-
-# Modifying LDFLAGS directly is confusing and deprecated,
-# but we keep the old behaviour during the transition period
-# because some bug work-arounds rely on --as-needed.
-
-# Avoid dpkg-shlibdeps warning about depending on a library from which
-# no symbol is used, see http://wiki.debian.org/ToolChain/DSOLinking.
-# Gnatmake users must upgrade to >= 4.6.4-1 to circumvent #680292.
-comma := ,
-ifeq (,$(filter -Wl$(comma)--as-needed,$(LDFLAGS)))
-  $(warning adding -Wl,--as-needed to LDFLAGS for compatibility, \
-      but please use DEB_LDFLAGS_MAINT_APPEND instead.)
-  LDFLAGS += -Wl,--as-needed
-  ifdef DPKG_EXPORT_BUILDFLAGS
-    export LDFLAGS
-  endif
-endif
-
-# Warn during build time if undefined symbols.
-ifeq (,$(filter -Wl$(comma)-z$(comma)defs -Wl$(comma)--no-undefined,$(LDFLAGS)))
-  $(warning adding -Wl,--no-undefined to LDFLAGS for compatibility, \
-     but please use DEB_LDFLAGS_MAINT_APPEND instead.)
-  LDFLAGS += -Wl,--no-undefined
-  ifdef DPKG_EXPORT_BUILDFLAGS
-    export LDFLAGS
-  endif
-endif
-
 ######################################################################
 # C compiler version
 
 # GCC binaries must be compatible with GNAT at the binary level, use
 # the same version. This setting is mandatory for every upstream C
-# compilation ("export CC" is enough for dh_auto_configure with a
-# normal ./configure).
+# compilation. Typical use:
+# override_dh_auto_configure:
+#         dh_auto_configure -- CC='$(CC)'
 
 CC := gnatgcc
 
@@ -105,7 +76,7 @@ CC := gnatgcc
 # Use all processors unless parallel=n is set in DEB_BUILD_OPTIONS.
 # http://www.debian.org/doc/debian-policy/ch-source.html#s-debianrules-options
 # The value may be useful elsewhere. Example: SPHINXOPTS=-j$(BUILDER_JOBS)
-BUILDER_JOBS := $(filter parallel=%,$(DEB_BUILD_OPTIONS))
+BUILDER_JOBS := $(filter parallel=%,$(DEB_BUILD_OPTIONS) $(DEB_BUILD_MAINT_OPTIONS))
 ifneq (,$(BUILDER_JOBS))
   BUILDER_JOBS := $(subst parallel=,,$(BUILDER_JOBS))
 else
@@ -117,7 +88,7 @@ BUILDER_OPTIONS += -R
 # Avoid lintian warning about setting an explicit library runpath.
 # http://wiki.debian.org/RpathIssue
 
-ifeq (,$(filter terse,$(DEB_BUILD_OPTIONS)))
+ifeq (,$(filter terse,$(DEB_BUILD_OPTIONS) $(DEB_BUILD_MAINT_OPTIONS)))
 BUILDER_OPTIONS += -v
 endif
 # Make exact command lines available for automatic log checkers.
