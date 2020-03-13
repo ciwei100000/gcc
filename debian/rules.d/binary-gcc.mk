3e8201a7f91bc2ac9ffd8432ec5abe1ad84cdbd7
Matthias Klose <doko@ubuntu.com>
2020-03-13 09:40:33 +0100

    * Ship the include-fixed directory again, for a working #include <limits.h>.

--- debian/rules.d/binary-gcc.mk
+++ debian/rules.d/binary-gcc.mk
@@ -30,7 +30,7 @@ dirs_gcc = \
 	$(docdir)/$(p_xbase)/{gcc,libssp,gomp,itm,quadmath,sanitizer} \
 	$(PF)/bin \
 	$(gcc_lexec_dir) \
-	$(gcc_lib_dir)/include \
+	$(gcc_lib_dir)/{include,include-fixed} \
 	$(PF)/share/man/man1 $(libgcc_dir)
 
 # XXX: what about triarch mapping?
@@ -52,7 +52,13 @@ files_gcc += \
 
 ifeq ($(DEB_STAGE),stage1)
     files_gcc += \
-	$(gcc_lib_dir)/include
+	$(gcc_lib_dir)/include \
+	$(shell for h in \
+		  README limits.h syslimits.h; \
+		do \
+		  test -e $(d)/$(gcc_lib_dir)/include-fixed/$$h \
+		    && echo $(gcc_lib_dir)/include-fixed/$$h; \
+		done)
 endif
 
 ifneq ($(GFDL_INVARIANT_FREE),yes)
