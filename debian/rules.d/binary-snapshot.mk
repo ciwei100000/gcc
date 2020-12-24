73ffe36e8f18737e8d22428d4f35f966c9e10f41
Matthias Klose <doko@ubuntu.com>
2020-12-24 08:28:17 +0000

    * Build the gcn offload compiler based on amdgcn-tools (based on LLVM 9).

--- debian/rules.d/binary-snapshot.mk
+++ debian/rules.d/binary-snapshot.mk
@@ -24,7 +24,11 @@ ifeq ($(with_offload_nvptx),yes)
   snapshot_depends += nvptx-tools,
 endif
 ifeq ($(with_offload_gcn),yes)
-  snapshot_depends += llvm-$(gcn_tools_llvm_version), lld-$(gcn_tools_llvm_version),
+ ifeq ($(gcn_tools_llvm_version),tools)
+   snapshot_depends += amdgcn-tools,
+ else
+   snapshot_depends += llvm-$(gcn_tools_llvm_version), lld-$(gcn_tools_llvm_version),
+ endif
 endif
 
 common_substvars += '-Vsnap:depends=$(snapshot_depends)' '-Vsnap:recommends=$(snapshot_recommends)'
@@ -103,6 +107,15 @@ ifeq ($(with_offload_gcn),yes)
 	tar -c -C $(d)-gcn -f - $(PF) \
 	  | tar x -C $(d_snap) -f -
 
+  ifeq ($(gcn_tools_llvm_version),tools)
+	: # re-create the symlinks as relative symlinks
+	dh_link -p$(p_snap) \
+	  /usr/$(gcn_target_name)/bin/ar     /$(gcc_lexec_dir)/accel/$(gcn_target_name)/ar \
+	  /usr/$(gcn_target_name)/bin/as     /$(gcc_lexec_dir)/accel/$(gcn_target_name)/as \
+	  /usr/$(gcn_target_name)/bin/ld     /$(gcc_lexec_dir)/accel/$(gcn_target_name)/ld \
+	  /usr/$(gcn_target_name)/bin/nm     /$(gcc_lexec_dir)/accel/$(gcn_target_name)/nm \
+	  /usr/$(gcn_target_name)/bin/ranlib /$(gcc_lexec_dir)/accel/$(gcn_target_name)/ranlib
+  else
 	: # re-create the symlinks as relative symlinks
 	dh_link -p$(p_snap) \
 	  /usr/lib/llvm-$(gcn_tools_llvm_version)/bin/llvm-ar /$(gcc_lexec_dir)/accel/$(gcn_target_name)/ar \
@@ -120,6 +133,7 @@ ifeq ($(with_offload_gcn),yes)
 	  /usr/lib/llvm-$(gcn_tools_llvm_version)/bin/llvm-ranlib /$(PF)/bin/$(gcn_target_name)-ranlib
 	rm -f $(d_snap)/$(PF)/bin/*-lto-dump
 	rm -f $(d_snap)/$(PF)/share/man/man1/*-accel-$(gcn_target_name)-*.1
+  endif
 endif
 
 ifeq ($(with_hppa64),yes)
